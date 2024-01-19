874ff1ea394fb45d39a4027556268f1e66fa768c
Crowdbotics <team@crowdbotics.com>
2024-01-19 17:41:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tee-timer-45450",
+  url: "https://tee-timer-45450.botics.co",
+  api: "https://tee-timer-45450.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
